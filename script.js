3a384377e96315edd5af790d08fe2a02bf022d06
Shipy4kaRU <moloko2004goda@gmail.com>
2024-08-25 08:36:37 +0400

    create project

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--temp');
+const inputElevation = document.querySelector('.form__input--climb');
