265ffcd72a36b024f38d10aa31d73da829e285db
Shipy4kaRU <moloko2004goda@gmail.com>
2024-08-25 09:41:45 +0400

    add leaflet map to the page

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  }),
+    function () {
+      alert('Ваша геолокация не смогла быть загружена');
+    };
+}
